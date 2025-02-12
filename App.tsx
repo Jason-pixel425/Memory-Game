a1189520d3994ad21abab58aacf9c34fec7e1687
Jason-pixel425 <jasonfitzg2798@gmail.com>
2025-02-12 21:41:40 +1000

    Added loading state to disable button
    Disables button when loading is true to prevent multiple calls to API
    
    Also fixed/added some more typing to button, form and startGame function

--- App.tsx
+++ App.tsx
@@ -3,33 +3,36 @@ import React, { useState } from 'react'
 import Form from './components/Form'
 import MemoryCard from './components/MemoryCard'
 
+interface Data {
+    name: string
+    category: string
+    group: string
+    htmlCode: Array<string>
+    unicode: Array<string>
+}
+
 export default function App() {
     // staet to track if game has started
     const [isGameOn, setIsGameOn] = useState<boolean>(false)
+    const [isLoading, setIsLoading] = useState<boolean>(false)
 
-        /**
-     * Challenge:
-     * 1) Turn startGame into an async function.
-     * 2) Use the try...catch syntax and make a fetch request to the emoji API, using this url:         "https://emojihub.yurace.pro/api/all/category/animals-and-nature". Store the response in a      const "response".
-     * 3) Check if the response is ok.
-     *      a) If yes, store the fetched data in a const "data". Log the data to the console.               Call setIsGameOn(true).
-     *      b) If no, throw an error with a custom error message, and inside the catch block, log           the error message to the console.
-     * 💡 Hint: Remember the await keyword!
-     * ⚠️ Warning: The emojis rendered will still be those from the hardcoded array.
-     */
+
     
     
     // function to flip isGameOn to true on game start
-     async function startGame(e) {
+     async function startGame(e: React.MouseEvent<HTMLButtonElement>): Promise<void> {
         e.preventDefault()
+        setIsLoading(true)
         try{
-           const response = await fetch("https://emojihub.yurace.pro/api/all/category/animals-and-nature")
-           const data = await response.json()
+           const response: Response = await fetch("https://emojihub.yurace.pro/api/all/category/animals-and-nature")
+           const data: Array<Data> = await response.json()
+           setIsGameOn(true)
            console.log(data)
-        } catch (e){
+        } catch (e: unknown){
             console.error("You got the error: ", e)
+        } finally {
+            setIsLoading(false)
         }
-        setIsGameOn(true)
     }
     
     // Function to turn a card over (currently just logging to console on click)
@@ -40,7 +43,7 @@ export default function App() {
     return (
         <main>
             <h1>Memory</h1>
-            {!isGameOn && <Form handleSubmit={startGame} />}
+            {!isGameOn && <Form isLoading={isLoading} handleSubmit={startGame} />}
             {isGameOn && <MemoryCard handleClick={turnCard} />}
         </main>
     )
--- components/Form.tsx
+++ components/Form.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import RegularButton from './RegularButton'
 
-export default function Form({ handleSubmit }) {
+interface Props {
+    handleSubmit: (e: React.MouseEvent<HTMLButtonElement>) => Promise<void>
+    isLoading: boolean
+}
+
+export default function Form({ handleSubmit, isLoading }: Props) {
     //Returns a form with a button to start the game ()
     return (
         <form className="wrapper">
-            <RegularButton handleClick={handleSubmit}>
+            <RegularButton isLoading={isLoading} handleClick={handleSubmit}>
                 Start Game
             </RegularButton>
         </form>
