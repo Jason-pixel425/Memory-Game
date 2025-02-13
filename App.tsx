8605095d4196f6a9c5f0a7d9750fdb6428b14364
Jason-pixel425 <jasonfitzg2798@gmail.com>
2025-02-13 15:44:51 +1000

    Added additional err handling and isntructions for next step

--- App.tsx
+++ App.tsx
@@ -17,7 +17,16 @@ export default function App() {
     const [isLoading, setIsLoading] = useState<boolean>(false)
 
 
-    
+        /**
+     * Challenge:
+     * 1) Create a new state variable, "emojisData", with a corresponding setter function, and initialize it as an empty array.
+     * 2) Inside the try block of the startGame function, create a new variable, "dataSample", and set it equal to the first 5 elements from "data".
+     * 3) Store the "dataSample" as "emojisData" in state.
+     * 4) Log "emojisData" to the console.
+     * 
+     * 💡 Hint: In step 2, use the JavaScript .slice() method to get the data sample.
+     * ⚠️ Warning: We're still rendering our hardcoded emojis.
+     */
     
     // function to flip isGameOn to true on game start
      async function startGame(e: React.MouseEvent<HTMLButtonElement>): Promise<void> {
@@ -25,6 +34,11 @@ export default function App() {
         setIsLoading(true)
         try{
            const response: Response = await fetch("https://emojihub.yurace.pro/api/all/category/animals-and-nature")
+           
+           if (!response.ok) {
+            throw new Error("Could not fetch data from API")
+           }
+           
            const data: Array<Data> = await response.json()
            setIsGameOn(true)
            console.log(data)
