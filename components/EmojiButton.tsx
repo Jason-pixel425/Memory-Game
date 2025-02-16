2a9ea85d0c7c719d4474042cace075a31e90a7a8
Jason-pixel425 <jasonfitzg2798@gmail.com>
2025-02-16 20:02:45 +1000

    Added aria labels to buttons/cards

--- components/EmojiButton.tsx
+++ components/EmojiButton.tsx
@@ -12,12 +12,15 @@ interface Props {
 export default function EmojiButton({handleClick, content, emojiName, index, selectedCardEntry, matchedCardEntry }: Props) {
     const disabled: boolean = selectedCardEntry || matchedCardEntry ? true : false
     const btnStyle: string =  matchedCardEntry ? "btn--emoji__back--matched" : selectedCardEntry ? "btn--emoji__back--selected" : "btn--emoji__front"
+    const btnAria: string = matchedCardEntry ? "Matched" : selectedCardEntry ? "Not Matched yet" : "Card upside down"
+    // User needs to know if card is selected, card is matched, or card is hidden. Card name
     return (
         <button
             className={`btn btn--emoji ${btnStyle}`}
-            onClick={(e) => handleClick(emojiName, index)}
+            onClick={() => handleClick(emojiName, index)}
             disabled={disabled}
-            
+            aria-label={`Position ${index + 1}: ${disabled ? emojiName : "unknown"}. ${btnAria}`}
+            aria-live="polite"
         >
             {selectedCardEntry || matchedCardEntry ? content : "?"}
         </button>
