985b2ea71b42289549951ad38f38daf927440404
Jason-pixel425 <jasonfitzg2798@gmail.com>
2025-02-20 16:58:20 +1000

    Added focus on GameOver component when it appears

--- components/GameOver.tsx
+++ components/GameOver.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+
 import RegularButton from './RegularButton.tsx'
 
 interface Props {
@@ -6,8 +7,14 @@ interface Props {
 }
 
 export default function GameOver({resetGame}: Props) {
+    const divRef = useRef<HTMLDivElement | null>(null)
+
+    useEffect(() => {
+        divRef.current?.focus()
+    }, [])
+
     return (
-        <div className="wrapper wrapper--accent">
+        <div ref={divRef} className="wrapper wrapper--accent" tabIndex={-1}>
             <p className="p--large">You've matched all the memory cards!</p>
             <RegularButton handleClick={resetGame}>Play again?</RegularButton>
         </div>
