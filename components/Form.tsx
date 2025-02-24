58a20336e12efe9253fcc6736cce1b1437652553
Jason-pixel425 <jasonfitzg2798@gmail.com>
2025-02-24 15:14:38 +1000

    Added labels to select elements and removed unecessary comments

--- components/Form.tsx
+++ components/Form.tsx
@@ -10,21 +10,12 @@ interface Props {
 export default function Form({ handleSubmit, handleChange, isLoading }: Props) {
     //Returns a form with a button to start the game ()
     
-       /**
-     * Challenge:
-     * 1) Create two select elements: one for emoji category and one for number of memory cards. 
-     * Each select element should have a name, an id and an onChange event handler. Use the following values:
-     *      - Category: name="category", id="category", onChange={handleChange}
-     *      - Number of cards: name="number", id="number", onChange={handleChange}
-     * 2) Inside of each select element, create five option elements, using the following values:
-     *      - Category: "animals-and-nature", "food-and-drink", "travel-and-places", "objects", "symbols"
-     *      - Number: 10, 20, 30, 40, 50
-     * 3) Give each select element a label describing the select menu.
-     */
+
     return (
         <div className="form-container">
             <form className="wrapper">
                 <div className="form__inner-wrapper">
+                    <label htmlFor="category">Select a category</label>
                     <select name="category" id="category" onChange={handleChange}>
                         <option value="animals-and-nature">Animals and Nature</option>
                         <option value="food-and-drink">Food and Drink</option>
@@ -34,6 +25,7 @@ export default function Form({ handleSubmit, handleChange, isLoading }: Props) {
                     </select>
                 </div>
                 <div className="form__inner-wrapper">
+                    <label htmlFor="number">Select number of pairs</label>
                     <select name="number" id="number" onChange={handleChange}>
                         <option value={10}>10</option>
                         <option value={20}>20</option>
